9be948c796fa21320d6b3a597f2ae6356a8ba9dd
aman282571 <aman282571@gmail.com>
2021-06-22 19:24:15 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Loadproblems from "./Loadproblems";
+import "./App.css";
 
 function App() {
+  const [user, setuser] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {user === null ? (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            setuser(e.target.user.value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>Enter Codeforces handle</p>
+          <input id="user" placeholder="Enter Codeforces handle" />
+        </form>
+      ) : (
+        <Loadproblems User={user} />
+      )}
     </div>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./loader.css";
+function Loader(props) {
+  return (
+    <div id="loader">
+      <div id="box">
+        <div id="l">
+          <p>L</p>
+          <div></div>
+        </div>
+        <div id="o">
+          <p>O</p>
+          <div></div>
+        </div>
+        <div id="a">
+          <p>A</p>
+          <div></div>
+        </div>
+        <div id="d">
+          <p>D</p>
+          <div></div>
+        </div>
+        <div id="i">
+          <p>I</p>
+          <div></div>
+        </div>
+        <div id="n">
+          <p>N</p>
+          <div></div>
+        </div>
+        <div id="g">
+          <p>G</p>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
